de87fe4616978e180e3b6f494185e8812bb732c0
njy1213 <njy1213@njy1213-Macbook.local>
2023-03-26 20:49:49 +0900

    feat: TODO List, Item Svg UI 작업

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,17 +1,25 @@
 import { Platform, SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import InputForm from '../components/InputForm'
+import TodoItem from '../components/TodoItem'
 
 const MainScreen = () => {
     return (
+        // IOS 11 이상에 적용되고 IOS 기종의 상단 부분 안전 경계 부분 조작
         <SafeAreaView style={styles.container}>
+            {/* 상태표시줄 제어 */}
             <StatusBar barStyle={'dark-content'} />
-            <Text>TODO APP</Text>
-            <View>
-                <Text>할 일</Text>
+            <Text style={styles.pageTitle}>TODO APP</Text>
+            <View style={styles.listView}>
+                <Text style={styles.listTitle}>할 일</Text>
+                <TodoItem />
             </View>
-            <View>
-                <Text>완료된 일</Text>
+            <View style={styles.divider} />
+            <View style={styles.listView}>
+                <Text style={styles.listTitle}>완료된 일</Text>
+                <TodoItem />
             </View>
+            <InputForm />
         </SafeAreaView>
 
     )
@@ -29,6 +37,21 @@ const styles = StyleSheet.create({
         marginBottom: 35,
         paddingHorizontal: 15,
         fontSize: 40,
-        
+    },
+    divider: {
+        marginHorizontal: 10,
+        marginTop: 25,
+        marginBottom: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: 'rgba(0,0,0,0.2',
+    },
+    listView: {
+        flex: 1,
+    },
+    listTitle: {
+        marginBottom: 25,
+        paddingHorizontal: 15,
+        fontSize: 40,
+        fontWeight: '500',
     }
 })
